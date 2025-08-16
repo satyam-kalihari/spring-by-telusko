fa53d78173cd2f87c869627561b44d99d365a557
satyam-kalihari <satyamkalihari17@gmail.com>
2025-08-16 16:25:42 +0530

    GET & POST request

--- eshop/src/main/java/com/telusko/eshop/controller/ProductController.java
+++ eshop/src/main/java/com/telusko/eshop/controller/ProductController.java
@@ -1,7 +1,11 @@
 package com.telusko.eshop.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -17,5 +21,15 @@ public class ProductController {
     @RequestMapping("/products")
     public List<Product> getProducts(){
         return productService.getProducts();
+    }
+
+    @GetMapping("/products/{prodId}")
+    public Product getProductById(@PathVariable int prodId){
+        return productService.getProductById(prodId);
+    }
+
+    @PostMapping("/products")
+    public String addProduct(@RequestBody Product product){
+        return productService.addProduct(product) ? "Product added successfully" : "Product not added";
     }
 }
--- eshop/src/main/java/com/telusko/eshop/service/ProductService.java
+++ eshop/src/main/java/com/telusko/eshop/service/ProductService.java
@@ -1,5 +1,6 @@
 package com.telusko.eshop.service;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import org.springframework.stereotype.Service;
@@ -8,14 +9,28 @@ import com.telusko.eshop.models.Product;
 @Service
 public class ProductService {
 
-    List<Product> products = Arrays.asList(
+    List<Product> products = new ArrayList<Product>(Arrays.asList(
         new Product(101, "Laptop", 10, 1200.00),
         new Product(102, "Mouse", 50, 25.00),
         new Product(103, "Keyboard", 30, 75.00),
-        new Product(104, "Monitor", 15, 300.00)
+        new Product(104, "Monitor", 15, 300.00))
     );
 
     public List<Product> getProducts() {
         return products;
+    }
+
+    public Product getProductById(int prodId) {
+        
+        return products.stream().filter(p -> p.getProdId() == prodId).findFirst().get();
+    }
+
+    public boolean addProduct(Product product) {
+       try {
+        products.add(product);
+        return true;
+       } catch (Exception e) {
+        return false;
+       }
     }
 }
