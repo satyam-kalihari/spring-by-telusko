021f40b0231afb92531913eb9f69aff69428774f
satyam-kalihari <satyamkalihari17@gmail.com>
2025-08-18 20:25:03 +0530

    Spring jpa & H2 database

--- eshop/src/main/java/com/telusko/eshop/models/Product.java
+++ eshop/src/main/java/com/telusko/eshop/models/Product.java
@@ -1,12 +1,20 @@
 package com.telusko.eshop.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 
 @Data
 @AllArgsConstructor
+@Entity
 public class Product {
+
+    public Product() {
+        //TODO Auto-generated constructor stub
+    }
+    @Id
     private int prodId;
     private String prodName;
     private int prodQut;
--- eshop/src/main/java/com/telusko/eshop/service/ProductService.java
+++ eshop/src/main/java/com/telusko/eshop/service/ProductService.java
@@ -1,66 +1,75 @@
 package com.telusko.eshop.service;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.telusko.eshop.models.Product;
+import com.telusko.eshop.repository.ProductRepo;
 
 @Service
 public class ProductService {
 
-    List<Product> products = new ArrayList<Product>(Arrays.asList(
-        new Product(101, "Laptop", 10, 1200.00),
-        new Product(102, "Mouse", 50, 25.00),
-        new Product(103, "Keyboard", 30, 75.00),
-        new Product(104, "Monitor", 15, 300.00))
-    );
+    @Autowired
+    ProductRepo repo;
+
+    // List<Product> products = new ArrayList<Product>(Arrays.asList(
+    //     new Product(101, "Laptop", 10, 1200.00),
+    //     new Product(102, "Mouse", 50, 25.00),
+    //     new Product(103, "Keyboard", 30, 75.00),
+    //     new Product(104, "Monitor", 15, 300.00))
+    // );
 
     public List<Product> getProducts() {
-        return products;
+        return repo.findAll();
     }
 
     public Product getProductById(int prodId) {
         
-        return products.stream().filter(p -> p.getProdId() == prodId).findFirst().get();
+        // return products.stream().filter(p -> p.getProdId() == prodId).findFirst().get();
+        return repo.findById(prodId).orElse(new Product());
     }
 
     public boolean addProduct(Product product) {
-       try {
-        products.add(product);
-        return true;
-       } catch (Exception e) {
-        return false;
-       }
+    //    try {
+    //     products.add(product);
+    //     return true;
+    //    } catch (Exception e) {
+    //     return false;
+    //    }
+    return repo.save(product) != null;
     }
 
-    private int getIndex(int prodId){
-        int index = -1;
-        for (int i = 0; i < products.size(); i++) {
-            if (products.get(i).getProdId() == prodId) {
-                index = i;
-                break;
-            }
-        }
-        return index;
-    }
+    // private int getIndex(int prodId){
+    //     int index = -1;
+    //     for (int i = 0; i < products.size(); i++) {
+    //         if (products.get(i).getProdId() == prodId) {
+    //             index = i;
+    //             break;
+    //         }
+    //     }
+    //     return index;
+    // }
 
     public String updateProduct(int prodId, Product product) {
         
-        int index = getIndex(prodId);
-        if (index == -1) {
-            return "Product not found";
-        }
-        products.set(index, product);
+        // int index = getIndex(prodId);
+        // if (index == -1) {
+        //     return "Product not found";
+        // }
+        // products.set(index, product);
+        repo.save(product);
         return "Product updated successfully";
     }
 
     public String deleteProduct(int prodId){
-        int index = getIndex(prodId);
-        if(index == -1){
-            return "Product not found";
-        }
-        products.remove(index);
+        // int index = getIndex(prodId);
+        // if(index == -1){
+        //     return "Product not found";
+        // }
+        // products.remove(index);
+
+        repo.deleteById(prodId);
         return "Product deleted successfully";
     }
 }
